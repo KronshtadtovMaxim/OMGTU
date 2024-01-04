866ce4243da1de47bfc5ad4a7bbbfedf8519ce6c
KronshtadtovMaxim <maks.kronshtadtov@mail.ru>
2024-01-04 23:32:45 +0600

    Create Program.cs

---
+++ Олимпиадки/Грядки/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите количество грядок:");
+int n = int.Parse(Console.ReadLine());
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int l = int.Parse(Console.ReadLine());
+int s = 0;
+for (int i = 0; i < n; i++) //через цикл
+{
+    s += x * (2 + 2*i) + 2 * y + 2 * l;
+        }
+Console.WriteLine(s);
+int S = 2 * n * (l + x + y) + x * n * (n - 1); //через формулу
+Console.WriteLine(S);
