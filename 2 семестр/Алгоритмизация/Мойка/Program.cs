fac3efe80ffcb5d1c17b1348fda6ce6f3d8dc066
KronshtadtovMaxim <maks.kronshtadtov@mail.ru>
2024-05-29 19:55:31 +0600

    Create Program.cs

---
+++ 2 семестр/Алгоритмизация/Мойка/Program.cs
@@ -0,0 +1,52 @@
+﻿public delegate void Wash(Car car);
+public class Car
+{
+    public string name;
+    public string number;
+    public Car(string name, string number)
+    {
+        this.name = name;
+        this.number = number;
+    }
+
+}
+class Moyka
+{
+    public void clean_car(Wash wash, Car car)
+    {
+        wash(car);
+    }
+}
+public class Garage
+{
+    public List<Car> cars = new();
+    public void Add(Car car) { cars.Add(car); }
+    public void washall(Wash wash)
+    {
+        foreach(Car car in cars) 
+        {
+            wash(car);
+        }
+    }
+         
+}
+class Program
+{
+    static void Main()
+    {
+        Garage cars = new();
+        while (true)
+        {
+            Console.Write("Введите название и номер машины через пробел: ");
+            string[] a = Console.ReadLine().Split(" ");
+            if (a[0] == "end") { break; }
+            cars.Add((new Car(a[0], a[1])));
+        }
+        cars.washall(car => Console.WriteLine($"{car.name} {car.number} была вымыта"));
+    }
+}
+
+
+
+
+
