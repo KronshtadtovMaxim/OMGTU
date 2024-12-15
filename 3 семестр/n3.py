d057c23e5ba15bc61d78f9077bb67ace912cb17f
KronshtadtovMaxim <145259097+KronshtadtovMaxim@users.noreply.github.com>
2024-12-16 02:06:41 +0600

    Update n3.py

--- 3 семестр/n3.py
+++ 3 семестр/n3.py
@@ -3,6 +3,7 @@ import scipy
 from scipy.linalg import lu
 from scipy import stats
 matrix = numpy.array([[2.3, 0,-3.4, -12], [2.6, 8.4, 0, -6], [1.3, 4.5, -17, 2], [1.8, 0, 15, 16]])
+print(matrix)
 P, L, U = lu(matrix)
 print("Матрица L (нижняя треугольная):")
 print(L)
@@ -12,7 +13,7 @@ print("Матрица P (перестановок):")
 print(P)
 print()
 
-det_U = numpy.prod(numpy.diag(U)) #т.к. она треугольная
+det_U = numpy.prod(numpy.diag(U)) #произведение элементов по главной диагонали (поиск определителя в диагональной матрице)
 #det L = 1
 det_P_inv = 1/numpy.linalg.det(P)
 det_matrix = det_P_inv * 1 * det_U
@@ -20,7 +21,7 @@ print("Определитель матрицы matrix: ", det_matrix)
 
 
 a = numpy.random.randint(0, 100, 100) #равномерное
-b = numpy.random.normal(50, 100/6, 100) #normal
+b = numpy.random.normal(50, 20, 100) #normal
 b = numpy.clip(numpy.round(b), 0, 100).astype(int) #округление
 print("Равномерная выборка: ")
 print(a)
@@ -45,10 +46,13 @@ print("Стандартное отклонение: ", numpy.std(b, ddof = 1))
 print()
 
 aa = [1]*100 #Ожидаемые частоты
-b = [0]*100
-#Воспользуемся равномерной выборкой
+b = [0]*100 #Заготовка для наблюдаемых частот
+#Воспользуемся равномерной выборкой из номера 4
 for n in a:
     b[n] += 1
 statistic, pvalue = scipy.stats.chisquare(b,aa)
 print(f"p-value: {pvalue}")
-print("0.94 > 0.05 => распределение выборки равномерное")
+if pvalue > 0.05:
+    print("Распределение выборки равномерное")
+else:
+    print("Распределение выборки не равномерное")
