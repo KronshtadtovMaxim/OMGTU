580c1eb470a7f245edda232c5bdfa851f0fd3b4d
KronshtadtovMaxim <145259097+KronshtadtovMaxim@users.noreply.github.com>
2024-12-16 01:45:21 +0600

    Update 2ex.py

--- 3 семестр/2ex.py
+++ 3 семестр/2ex.py
@@ -3,7 +3,7 @@ import sympy as sp
 from scipy import special
 from scipy import integrate
 import numpy as np
-from scipy import optimize
+from scipy.optimize import minimize, NonlinearConstraint
 x = sp.Symbol('x')
 y = 2/(sp.sin(x) + 4)
 derivative = sp.diff(y, x)
@@ -23,12 +23,15 @@ print()
 def func(x):
     x1, x2 = x
     return (x1 - 4)**2 + (x2 - 2)**2
+#Ограничения
+def constraint1(x):
+    return 4 * x[0] + 2 * x[1]
 
-ogr = [
-    {'type': 'ineq', 'fun': lambda x: 4*x[0] + 2*x[1] - 11},  # 4x1 + 2x2 >= 11
-    {'type': 'ineq', 'fun': lambda x: -2*x[0] + 7}            # -2x1 >= 7
-]
-x0 = np.array([0,0]) # все переменные неотрициательны
-result = scipy.optimize.minimize(func, x0, constraints=ogr)
+def constraint2(x):
+    return -2 * x[0]
+a = NonlinearConstraint(constraint1, 11, np.inf); #NonlinearConstraint(constraint1, 11 (7), np.inf) создает объект ограничения, где constraint1 — это функция,
+b = NonlinearConstraint(constraint2, 7, np.inf); #которая должна быть неотрицательной, а 11 или 7 и np.inf указывают на то, что функция должна быть больше 11 или 7.
+x0 = np.array([0,0]) # так как все переменные неотрицательны, начинаем поиск с x1 = 0 и x2 = 0
+result = minimize(func, x0, constraints=[a, b])
 print("Оптимальное решение: ", result.x)
 print("Минимальное значение функции: ", result.fun)
